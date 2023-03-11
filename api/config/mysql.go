4ed649b9c29ac326b188b89c87d3b55176be03de
leeguo <leeguo2015@163.com>
2023-03-10 18:01:28 -0800

    gorm init

--- api/config/mysql.go
+++ api/config/mysql.go
@@ -1,10 +1,16 @@
 package config
 
 type Mysql struct {
-	Host      string `yaml:"host"`
-	Port      string `yaml:"port"`
-	DB        string `yaml:"db"`
-	User      string `yaml:"user"`
-	Password  string `yaml:"password"`
-	LoggLevel string `yaml:"level"`
+	Host         string `yaml:"host"`
+	Port         string `yaml:"port"`
+	DB           string `yaml:"db"`
+	User         string `yaml:"user"`
+	Password     string `yaml:"password"`
+	LoggLevel    string `yaml:"level"`
+	MaxIdleConns int    `yaml:"max_idle_conns"`
+	MaxOpenConns int    `yamlL:maxopen`
+}
+
+func (m *Mysql) Dsn() string {
+	return m.User + ":" + m.Password + "@tcp(" + m.Host + ":" + m.Port + ")/" + m.DB + "?charset=utf8mb4&parseTime=True&loc=Local"
 }
