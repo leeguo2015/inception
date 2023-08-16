5dd3a9c8019644af9342954d459a82146e2f9037
v_liguo <Leeguo2015@163.com>
2023-08-16 19:44:11 +0800

    [feat] comment

--- api/internal/logic/blog/comment.go
+++ api/internal/logic/blog/comment.go
@@ -1,22 +1,63 @@
 package blog
 
-import "inception/api/internal/model"
+import (
+	"fmt"
+	"inception/api/internal/global"
+	"inception/api/internal/model"
+
+	"gorm.io/gorm/clause"
+)
 
 type CommentType string
 
 var CommentTypeBlog CommentType = "blog"
 var CommentTypeComment CommentType = "comment"
 
-func CommentAdd(commentType CommentType, comment string， UserID, uint) error {
+func CommentAdd(commentType CommentType, Comtent string, UserID, OwnerID uint) error {
+	comment := new(model.Comment)
+	switch commentType {
+	case CommentTypeBlog:
+		comment.BlogID = OwnerID
+	case CommentTypeComment:
+		comment.ParentID = OwnerID
+	default:
+		return fmt.Errorf("unsupported comment type %s", commentType)
+	}
+	comment.Comtent = Comtent
+	comment.UserID = UserID
+
+	if err := global.DB.Create(comment).Error; err != nil {
+		return err
+	}
 	return nil
 }
 
-func CommentDelete() {
+func CommentDelete(CommentID uint) error {
+	comment := new(model.Comment)
+	comment.ID = CommentID
+	err := global.DB.Select(clause.Associations).Delete(comment).Error
+	if err != nil {
+		return fmt.Errorf("delete comment failed %s", err)
+	}
+	return nil
 
 }
 
-func CommentGet(blogID uint, start, limit int) (model.Comment, int, error) {
-	Comment := model.Comment{}
+func CommentGet(blogID uint, start, limit int) ([]model.Comment, int, error) {
+	comments := []model.Comment{}
+	total := 0
 
-	return Comment, 0, nil
+	// 查询符合条件的评论
+	err := db.Where("blog_id = ?", blogID).Offset(start).Limit(limit).Find(&comments).Error
+	if err != nil {
+		return nil, total, err
+	}
+
+	// 查询总评论数
+	err = db.Model(&model.Comment{}).Where("blog_id = ?", blogID).Count(&total).Error
+	if err != nil {
+		return nil, total, err
+	}
+
+	return comments, total, nil
 }
