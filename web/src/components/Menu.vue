16a1c9a9a73b8903807c62a33c7f1ff688d39a03
leeguo2015 <56671982+leeguo2015@users.noreply.github.com>
2023-09-06 00:17:16 +0800

    Web (#12)
    * [feat] 零碎东西
    
    * [feat] 布局
    
    * [feat] 登录框

--- web/src/components/Menu.vue
+++ web/src/components/Menu.vue
@@ -1,42 +1,62 @@
 <template>
-    <el-menu :default-active="activeIndex" class="el-menu-demo padding1" mode="horizontal" background-color="#545c64"
+    <el-menu :default-active="activeIndex" class="el-menu-demo padding1" mode="horizontal" background-color="#617172"
         text-color="#fff" :ellipsis="false" @select="handleSelect">
 
         <el-menu-item index="0">
-            HOME <el-icon>
+            <router-link to="/">主页</router-link> <el-icon>
                 <House />
+
             </el-icon>
         </el-menu-item>
-        <el-menu-item index="2" >
-            VIEW <el-icon><DataBoard /></el-icon>
+        <el-menu-item index="2">
+            文章 <el-icon>
+                <DataBoard />
+            </el-icon>
         </el-menu-item>
         <div class="flex-grow" />
-        <el-menu-item index="3">SEARCH <el-icon>
+        <el-menu-item index="3">
+            搜索 
+            <el-icon>
                 <Search />
-            </el-icon></el-menu-item>
+            </el-icon>
+        </el-menu-item>
         <el-sub-menu index="4">
-            <template #title>USER <el-icon><User/></el-icon></template>
-            <el-menu-item index="2-1">信息更改</el-menu-item>
-            <el-menu-item index="2-1">文章管理</el-menu-item>
+            <template #title><router-link to="/login">个人信息 </router-link><el-icon>
+                    <User />
+                </el-icon></template>
+            <el-menu-item index="2-1">个人信息</el-menu-item>
+            <el-menu-item index="2-1">注销</el-menu-item>
         </el-sub-menu>
     </el-menu>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
-
-const activeIndex = ref('1')
-const handleSelect = (key: string, keyPath: string[]) => {
-    console.log(key, keyPath)
-}
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
 </script>
 
 <style>
 .padding1 {
     padding: 0 1rem;
-    ;
+    width: 100%;
+    border-bottom: solid 0px var(--el-border-color-base)!important;
+
 }
 
 .flex-grow {
     flex-grow: 1;
-}</style>
+}
+
+
+</style>
